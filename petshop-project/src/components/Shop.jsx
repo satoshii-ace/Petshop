221db045b9cd201e231cad9082a19cf4bf256c1d
satoshiiTv <148063396+satoshii-ace@users.noreply.github.com>
2025-06-12 23:09:56 +0800

    working shop

---
+++ petshop-project/src/components/Shop.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import shop from '../assets/pet.png'
+import pet1 from '../assets/pet1.jpg'
+import pet2 from '../assets/pet2.jpg'
+import pet3 from '../assets/pet3.jpg'
+import pet4 from '../assets/pet4.jpg'
+import pet5 from '../assets/pet5.jpg'
+import pet6 from '../assets/pet6.jpg'
+
+const Shop = () => {
+    const shop = [
+        {id: 1, name:'Cat', price: 99.99, rating: 4.5, image:pet, discount: 20},
+        {id: 2, name:'Cat', price: 99.99, rating: 4.5, image:pet1, discount: 20},
+        {id: 3, name:'Cat', price: 99.99, rating: 4.5, image:pet2, discount: 20},
+        {id: 4, name:'Cat', price: 99.99, rating: 4.5, image:pet3, discount: 20},
+        {id: 5, name:'Cat', price: 99.99, rating: 4.5, image:pet4, discount: 20},
+        {id: 6, name:'Cat', price: 99.99, rating: 4.5, image:pet5, discount: 20},
+        {id: 7, name:'Cat', price: 99.99, rating: 4.5, image:pet6, discount: 20}
+    ]
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Shop
